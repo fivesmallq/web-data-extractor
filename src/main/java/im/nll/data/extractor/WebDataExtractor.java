e17bbd2a4d8d807c888b710c40ce14043f101e53
fivesmallq <fivesmallq@gmail.com>
2015-12-31 11:27:20 +0800

    fix typo

--- src/main/java/im/nll/data/extractor/WebDataExtractor.java
+++ src/main/java/im/nll/data/extractor/WebDataExtractor.java
@@ -93,7 +93,7 @@ public class WebDataExtractor {
             try {
                 Reflect.on(entity).set(name, data);
             } catch (Exception e) {
-                LOGGER.error("conver to bean error! can't set '{}' with '{}'", name, data, e);
+                LOGGER.error("convert to bean error! can't set '{}' with '{}'", name, data, e);
             }
         }
         return entity;
@@ -110,7 +110,7 @@ public class WebDataExtractor {
                 try {
                     Reflect.on(entity).set(name, data);
                 } catch (Exception e) {
-                    LOGGER.error("conver to bean error! can't set '{}' with '{}'", name, data, e);
+                    LOGGER.error("convertt to bean error! can't set '{}' with '{}'", name, data, e);
                 }
             }
             entityList.add(entity);
