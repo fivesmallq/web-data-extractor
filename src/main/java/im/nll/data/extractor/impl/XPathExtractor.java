b8cde6a026d1b2039b534e4c439b7015eee6dfc1
fivesmallq <fivesmallq@gmail.com>
2016-01-07 13:06:18 +0800

    fixed #7

--- src/main/java/im/nll/data/extractor/impl/XPathExtractor.java
+++ src/main/java/im/nll/data/extractor/impl/XPathExtractor.java
@@ -1,20 +1,93 @@
 package im.nll.data.extractor.impl;
 
-import im.nll.data.extractor.Extractor;
+import com.google.common.collect.Lists;
+import im.nll.data.extractor.ListableExtractor;
+import im.nll.data.extractor.utils.Logs;
+import org.jdom2.*;
+import org.jdom2.filter.Filters;
+import org.jdom2.input.SAXBuilder;
+import org.jdom2.output.Format;
+import org.jdom2.output.XMLOutputter;
+import org.jdom2.xpath.XPathExpression;
+import org.jdom2.xpath.XPathFactory;
+import org.slf4j.Logger;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.List;
 
 /**
  * @author <a href="mailto:fivesmallq@gmail.com">fivesmallq</a>
  * @version Revision: 1.0
  * @date 15/12/25 下午9:26
  */
-public class XPathExtractor implements Extractor {
+public class XPathExtractor implements ListableExtractor {
+    private static final Logger LOGGER = Logs.get();
+    private String xpath;
 
-    public XPathExtractor(String query) {
-
+    public XPathExtractor(String xpath) {
+        this.xpath = xpath;
     }
 
     @Override
-    public String extract(String html) {
-        return null;
+    public String extract(String data) {
+        SAXBuilder sax = new SAXBuilder();
+        String result = "";
+        try {
+            Document doc = sax.build(new StringReader(data));
+            XPathFactory xpfac = XPathFactory.instance();
+            XPathExpression xp = xpfac.compile(xpath, Filters.fpassthrough());
+            Object text = xp.evaluateFirst(doc);
+            result = wrap(text);
+            return result;
+        } catch (JDOMException e) {
+            LOGGER.error("extract data error. xpath:{} html:{}", xpath, data, e);
+        } catch (IOException e) {
+            LOGGER.error("extract data error. xpath:{} html:{}", xpath, data, e);
+        }
+        return result;
     }
+
+    @Override
+    public List<String> extractList(String data) {
+        SAXBuilder sax = new SAXBuilder();
+        List<String> stringList = Lists.newLinkedList();
+        try {
+            Document doc = sax.build(new StringReader(data));
+            XPathFactory xpfac = XPathFactory.instance();
+            XPathExpression xp = xpfac.compile(xpath, Filters.fpassthrough());
+            List<Object> texts = xp.evaluate(doc);
+            for (Object text : texts) {
+                String result = wrap(text);
+                stringList.add(result);
+            }
+            return stringList;
+        } catch (JDOMException e) {
+            LOGGER.error("extract data error. xpath:{} html:{}", xpath, data, e);
+        } catch (IOException e) {
+            LOGGER.error("extract data error. xpath:{} html:{}", xpath, data, e);
+        }
+        return stringList;
+    }
+
+    private String wrap(Object text) {
+        if (text != null) {
+            if (text instanceof Attribute) {
+                return ((Attribute) text).getValue();
+            } else if (text instanceof Content) {
+                XMLOutputter xout = new XMLOutputter(Format.getPrettyFormat());
+                if (text instanceof Element) {
+                    return xout.outputString((Element) text);
+                }
+                if (text instanceof Text) {
+                    return xout.outputString((Text) text);
+                }
+            } else {
+                LOGGER.error("unsupported document type", text.getClass());
+            }
+        }
+        return "";
+    }
+
+
 }
---
+++ src/test/java/im/nll/data/extractor/impl/XPathExtractorTest.java
@@ -0,0 +1,67 @@
+package im.nll.data.extractor.impl;
+
+import com.google.common.base.Charsets;
+import com.google.common.io.Resources;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+/**
+ * @author <a href="mailto:fivesmallq@gmail.com">fivesmallq</a>
+ * @version Revision: 1.0
+ * @date 16/1/6 上午12:32
+ */
+public class XPathExtractorTest {
+    XPathExtractor xPathExtractor;
+    private String baseHtml;
+
+    @Before
+    public void setUp() throws Exception {
+        baseHtml = Resources.toString(Resources.getResource("base.html"), Charsets.UTF_8);
+    }
+
+    @Test
+    public void testExtract() throws Exception {
+        //attribute
+        xPathExtractor = new XPathExtractor("//div/a[1]/@href");
+        String s = xPathExtractor.extract(baseHtml);
+        Assert.assertEquals("/fivesmallq", s);
+        //element
+        xPathExtractor = new XPathExtractor("//div/a[1]");
+        s = xPathExtractor.extract(baseHtml);
+        Assert.assertEquals("<a href=\"/fivesmallq\" class=\"title\">fivesmallq</a>", s);
+        //text
+        xPathExtractor = new XPathExtractor("//div/a[1]/text()");
+        s = xPathExtractor.extract(baseHtml);
+        Assert.assertEquals("fivesmallq", s);
+    }
+
+    @Test
+    public void testExtractList() throws Exception {
+        //attribute
+        xPathExtractor = new XPathExtractor("//div/a/@href");
+        List<String> s = xPathExtractor.extractList(baseHtml);
+        Assert.assertNotNull(s);
+        Assert.assertEquals(2, s.size());
+        String second = s.get(1);
+        Assert.assertEquals("/fivesmallq/followers", second);
+
+        //element
+        xPathExtractor = new XPathExtractor("//div/a");
+        s = xPathExtractor.extractList(baseHtml);
+        Assert.assertNotNull(s);
+        Assert.assertEquals(2, s.size());
+        second = s.get(1);
+        Assert.assertEquals("<a href=\"/fivesmallq/followers\">29671 Followers</a>", second);
+
+        //text
+        xPathExtractor = new XPathExtractor("//div/a/text()");
+        s = xPathExtractor.extractList(baseHtml);
+        Assert.assertNotNull(s);
+        Assert.assertEquals(2, s.size());
+        second = s.get(1);
+        Assert.assertEquals("29671 Followers", second);
+    }
+}
