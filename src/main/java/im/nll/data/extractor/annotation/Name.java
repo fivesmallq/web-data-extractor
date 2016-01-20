9d9694b91c5a245c0a63f2a7530fe093f36e4e5a
fivesmallq <fivesmallq@gmail.com>
2016-01-20 17:15:37 +0800

    add name annotation to define extractor short name

---
+++ src/main/java/im/nll/data/extractor/annotation/Name.java
@@ -0,0 +1,12 @@
+package im.nll.data.extractor.annotation;
+
+/**
+ * define extractor short name
+ *
+ * @author <a href="mailto:fivesmallq@gmail.com">fivesmallq</a>
+ * @version Revision: 1.0
+ * @date 16/1/19 下午2:15
+ */
+public @interface Name {
+    String value();
+}
