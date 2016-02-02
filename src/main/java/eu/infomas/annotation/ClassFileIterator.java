e298b36767ffb1cb6c76c87e28256417129ce5c6
fivesmallq <fivesmallq@gmail.com>
2016-02-02 22:32:41 +0800

    depend on annotation detector by source

---
+++ src/main/java/eu/infomas/annotation/ClassFileIterator.java
@@ -0,0 +1,144 @@
+/* ClassFileIterator.java
+ *
+ * Created: 2011-10-10 (Year-Month-Day)
+ * Character encoding: UTF-8
+ *
+ ****************************************** LICENSE *******************************************
+ *
+ * Copyright (c) 2011 - 2013 XIAM Solutions B.V. (http://www.xiam.nl)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package eu.infomas.annotation;
+
+import java.io.*;
+import java.util.zip.ZipFile;
+
+/**
+ * {@code ClassFileIterator} is used to iterate over all Java ClassFile files available within
+ * a specific context.
+ * <p>
+ * For every Java ClassFile ({@code .class}) an {@link InputStream} is returned.
+ *
+ * @author <a href="mailto:rmuller@xiam.nl">Ronald K. Muller</a>
+ * @since annotation-detector 3.0.0
+ */
+final class ClassFileIterator implements ResourceIterator {
+
+    private final FileIterator fileIterator;
+    private final String[] pkgNameFilter;
+    private ZipFileIterator zipIterator;
+
+    /**
+     * Create a new {@code ClassFileIterator} returning all Java ClassFile files available
+     * from the class path ({@code System.getProperty("java.class.path")}).
+     */
+    ClassFileIterator() throws IOException {
+        this(classPath(), null);
+    }
+
+    /**
+     * Create a new {@code ClassFileIterator} returning all Java ClassFile files available
+     * from the specified files and/or directories, including sub directories.
+     * <p>
+     * If the (optional) package filter is defined, only class files staring with one of the
+     * defined package names are returned.
+     * NOTE: package names must be defined in the native format (using '/' instead of '.').
+     */
+    ClassFileIterator(final File[] filesOrDirectories, final String[] pkgNameFilter)
+            throws IOException {
+
+        this.fileIterator = new FileIterator(filesOrDirectories);
+        this.pkgNameFilter = pkgNameFilter;
+    }
+
+    /**
+     * Return the name of the Java ClassFile returned from the last call to {@link #next()}.
+     * The name is either the path name of a file or the name of an ZIP/JAR file entry.
+     */
+    public String getName() {
+        // Both getPath() and getName() are very light weight method calls
+        return zipIterator == null ?
+                fileIterator.getFile().getPath() :
+                zipIterator.getEntry().getName();
+    }
+
+    @Override
+    public InputStream next() throws IOException {
+        while (true) {
+            if (zipIterator == null) {
+                final File file = fileIterator.next();
+                // not all specified Files exists!
+                if (file == null || !file.isFile()) {
+                    return null;
+                } else {
+                    final String name = file.getName();
+                    if (name.endsWith(".class")) {
+                        return new FileInputStream(file);
+                    } else if (fileIterator.isRootFile() &&
+                            (endsWithIgnoreCase(name, ".jar") || isZipFile(file))) {
+                        zipIterator = new ZipFileIterator(new ZipFile(file), pkgNameFilter);
+                    } // else just ignore
+                }
+            } else {
+                final InputStream is = zipIterator.next();
+                if (is == null) {
+                    zipIterator = null;
+                } else {
+                    return is;
+                }
+            }
+        }
+    }
+
+    // private
+
+    private boolean isZipFile(final File file) {
+        DataInputStream in = null;
+        try {
+            in = new DataInputStream(new FileInputStream(file));
+            final int n = in.readInt();
+            return n == 0x504b0304;
+        } catch (IOException ex) {
+            // silently ignore read exceptions
+            return false;
+        } finally {
+            if (in != null) {
+                try {
+                    in.close();
+                } catch (IOException ex) {
+                    // ignore
+                }
+            }
+        }
+    }
+
+    /**
+     * Returns the class path of the current JVM instance as an array of {@link File} objects.
+     */
+    private static File[] classPath() {
+        final String[] fileNames =
+                System.getProperty("java.class.path").split(File.pathSeparator);
+        final File[] files = new File[fileNames.length];
+        for (int i = 0; i < files.length; ++i) {
+            files[i] = new File(fileNames[i]);
+        }
+        return files;
+    }
+
+    private static boolean endsWithIgnoreCase(final String value, final String suffix) {
+        final int n = suffix.length();
+        return value.regionMatches(true, value.length() - n, suffix, 0, n);
+    }
+
+}
