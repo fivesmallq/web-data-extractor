23f7026826dcce143b1412cd80ed6071168cad40
fivesmallq <fivesmallq@gmail.com>
2016-02-11 12:20:17 +0800

    add extract rule model

---
+++ src/main/java/im/nll/data/extractor/rule/ExtractRule.java
@@ -0,0 +1,29 @@
+package im.nll.data.extractor.rule;
+
+/**
+ * extract rule model.
+ *
+ * @author <a href="mailto:fivesmallq@gmail.com">fivesmallq</a>
+ * @version Revision: 1.0
+ * @date 16/2/11 下午12:18
+ */
+public class ExtractRule {
+    private String field;
+    private String extractor;
+
+    public String getField() {
+        return field;
+    }
+
+    public void setField(String field) {
+        this.field = field;
+    }
+
+    public String getExtractor() {
+        return extractor;
+    }
+
+    public void setExtractor(String extractor) {
+        this.extractor = extractor;
+    }
+}
