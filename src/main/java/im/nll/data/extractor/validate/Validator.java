ef97f548dffd06c09d91dbc8f5a4ea67b78cde50
fivesmallq <fivesmallq@gmail.com>
2016-05-01 14:30:10 +0800

    add Validator base

---
+++ src/main/java/im/nll/data/extractor/validate/Validator.java
@@ -0,0 +1,9 @@
+package im.nll.data.extractor.validate;
+
+/**
+ * @author <a href="mailto:fivesmallq@gmail.com">fivesmallq</a>
+ * @version Revision: 1.0
+ * @date 16/5/1 下午2:29
+ */
+public class Validator {
+}
